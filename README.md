4afa52dd4a206b15f29cc7832b73ece8421602e7
samarthchugh <samarthchugh049@gmail.com>
2025-12-05 15:10:41 +0000

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# LangGraph Basic Chatbot 
---
+++ langgraph_backend.py
@@ -0,0 +1,29 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage
+from langchain_groq import ChatGroq
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+import os
+load_dotenv()
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+llm = ChatGroq(api_key=GROQ_API_KEY, model="llama-3.1-8b-instant")
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+    
+def chat_node(state: ChatState) -> ChatState:
+    messages = state["messages"]
+    response = llm.invoke(messages)
+    return {'messages': [response]}
+
+checkpoint = InMemorySaver()
+
+graph = StateGraph(ChatState)
+graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpoint)
